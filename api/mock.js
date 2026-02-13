f25f26c98b91318ef370d9efd3623d263e237913
sx-9871 <linw@dtsphere.com>
2026-02-13 15:42:09 +0800

    Revert "fix: 突然出现的不知名添加更改，好像是依赖更新"
    This reverts commit 85fd48df43ea118bdf5ebe9a16eb8e2370690bee.

--- api/mock.js
+++ api/mock.js
@@ -1,41 +1,4 @@
 import Mock from 'mockjs'
 import pageApi from './mockData/pageApi'
 
-// 配置首页轮播图的 mock 数据
-Mock.mock(/api\/banners/, 'get', () => {
-    const result = pageApi.getBanner()
-    return {
-        code: 1,
-        data: result.data.bannerList
-    }
-})
-
-// 配置首页景点列表的 mock 数据
-Mock.mock(/api\/scenic-spots/, 'get', pageApi.getHomeList)
-
-// 配置喜欢列表的 mock 数据
-Mock.mock(/like\/list/, 'get', () => {
-    const result = pageApi.getHomeList()
-    return result.data
-})
-
-// 配置获取用户信息的 mock 数据
-Mock.mock(/getUserInfo/, 'get', () => {
-    return {
-        code: 1,
-        data: {
-            avatarUrl: 'https://img.yzcdn.cn/vant/cat.jpeg',
-            nickName: '游客用户'
-        }
-    }
-})
-
-// 配置登录的 mock 数据
-Mock.mock(/login/, 'get', () => {
-    return {
-        code: 1,
-        data: {
-            token: 'mock-token-' + Date.now()
-        }
-    }
-})
+Mock.mock(/api\/user\/getBanner/, 'get', pageApi.getBanner)
